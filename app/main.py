e2689a3661fc4b9c9ee39000793491923cd65a9d
Heinrich-Ressler <hprintr@gmail.com>
2025-04-01 07:03:09 +0200

    4. CRUD для регистрации и авторизации     - создал файлы : auth.py, security.py     - добавлен эндпоинт для регистрации (POST: /auth/register) принимает email, пароль; при успехе сохраняет пользователя в БД.     - добавлен эндпоинт для авторизации (POST: /auth/login) принимает email, пароль, выдаёт токен/сессию при успехе.     - При повторной регистрации с существующим email возвращается ошибка (400 )     - Пароли хранятся в хэшированном виде (bcrypt)     - в  main.py добавил rout на auth.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from . import posts
+from . import posts, auth
 
 
 app = FastAPI()
 
 app.include_router(posts.router)
+app.include_router(auth.router, prefix="/auth", tags=["Registration"])
 
 
